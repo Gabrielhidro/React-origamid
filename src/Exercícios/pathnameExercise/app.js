443b8e5aef2aa1aa56cdc0e55407be4b85c6545b
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-25 09:37:27 -0300

    useState

---
+++ src/Exercícios/pathnameExercise/app.js
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import Home from './components/Home'
+import Produtos from './components/Produtos'
+// Replique a interface como a apresentada na aula
+// Utilize a array abaixo para mostrar os produtos
+// Quebre em componentes o que precisar ser reutilizado
+// Dica: const { pathname } = window.location; (puxa o caminho do URL)
+
+
+export default function App(){
+  const { pathname } = window.location;
+
+  let Component;
+  if (pathname === '/produtos') {
+    Component = Produtos;
+  } else {
+    Component = Home;
+  }
+
+  return (
+    <>
+      <Component />
+    </>
+  )
+};
